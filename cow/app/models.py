e3345e71c1a6b8c66f3cf8881f0794af2740fa58
jwhite3 <b1gr3d92@gmail.com>
2022-06-09 11:32:12 -0400

    Add docstring and additional validation to Item model

--- cow/app/models.py
+++ cow/app/models.py
@@ -2,5 +2,9 @@ from pydantic import BaseModel
 
 
 class Item(BaseModel):
+    """Model that defines a valid Item."""
+    class Config:
+        validate_assignment = True  # Validate new data used in updating existing items
+
     id: int
     name: str
